f4c646bb77250cff01316327e54247f57cc49f2b
hua5 <zengxianghua5@163.com>
2017-05-02 17:23:18 +0800

    finish user edit update index and destroy actions

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -12,6 +12,13 @@ use Auth;
 //登录逻辑
 class SessionsController extends Controller
 {
+    public function __construct()
+    {  //只有未登录用户才可访问 create
+        $this->middleware('guest', [
+            'only' => ['create']
+        ]);
+    }
+
     public function create(){
         return view('sessions.create');
     }
@@ -29,7 +36,7 @@ class SessionsController extends Controller
         if (Auth::attempt($credentials,$request->has('remember'))){
            session()->flash('success', '欢迎回来！');
            
-           return redirect()->route('users.show', [Auth::user()]);
+           return redirect()->intended(route('users.show', [Auth::user()]));
         }else{
            session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
            return redirect()->back();
--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -10,6 +10,18 @@ use Auth;
 
 class UsersController extends Controller
 {
+
+    public function __construct()
+    {   //已登录可以进行的动作
+        $this->middleware('auth', [            
+            'only' => ['edit', 'update','destroy']
+        ]);
+       //游客可以进行的动作
+        $this->middleware('guest', [            
+            'only' => ['create']
+        ]);
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -17,7 +29,8 @@ class UsersController extends Controller
      */
     public function index()
     {
-        //
+        $users = User::paginate(30);
+        return view('users.index', compact('users'));
     }
 
     /**
@@ -76,7 +89,9 @@ class UsersController extends Controller
      */
     public function edit($id)
     {
-        //
+        $user = User::findOrFail($id);
+        $this->authorize('update', $user);
+        return view('users.edit',compact('user'));
     }
 
     /**
@@ -88,7 +103,22 @@ class UsersController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'password' => 'confirmed|min:6'
+        ]);
+
+        $user = User::findOrFail($id);
+        $this->authorize('update', $user);
+        $data = [];
+        if($request->password){
+            $data['password'] = bcrypt($request->password);
+        }
+        $user->update($data);
+        session()->flash('success', '个人资料更新成功！');
+
+
+        return redirect()->route('users.show', $id);
     }
 
     /**
@@ -99,6 +129,13 @@ class UsersController extends Controller
      */
     public function destroy($id)
     {
-        //
+
+        $user = User::findOrFail($id);
+        //对删除进行授权
+        $this->authorize('destroy', $user);
+        $user->delete();
+        session()->flash('success', '成功删除用户！');
+        return back();
+
     }
 }
