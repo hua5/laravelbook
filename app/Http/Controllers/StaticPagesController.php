fdcfd89bc3ab69d3ac41a3257202ad34842473d9
hua5 <zengxianghua5@163.com>
2017-05-05 12:43:52 +0800

    change some admin

--- app/Http/Controllers/StaticPagesController.php
+++ app/Http/Controllers/StaticPagesController.php
@@ -6,14 +6,16 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use Auth;
+use DB;
 
 class StaticPagesController extends Controller
 {
     public function home(){
         $feed_items = [];
-
+        DB::table('users') ->where('id', 1)->update(['votes' => 1]);
         if (Auth::check()) {
             $feed_items = Auth::user()->feed()->paginate(30);
+            
         }
 
         return view('static_pages/home',compact('feed_items'));
